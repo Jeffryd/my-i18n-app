5e6e822036fd04e34d6bd1876e74a1cad519be0b
jestrada <jafestrada21@gmail.com>
2023-12-18 14:35:43 -0600

    setup default translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,55 @@ import { NgModule, APP_INITIALIZER, LOCALE_ID } from '@angular/core';
 import { I18NextModule, ITranslationService, I18NEXT_SERVICE, I18NextTitle, defaultInterpolationFormat } from 'angular-i18next';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import Backend from 'i18next-chained-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// translation files
+import en from '../assets/locales/en.translations.json';
+import el from '../assets/locales/el.translations.json'
+
+const resources = {
+  en: {
+    translation: en
+  },
+  el: {
+    translation: el
+  }
+};
+
 export function appInit(i18next: ITranslationService) {
-  return () => i18next.init({
-      supportedLngs: ['en', 'gr'],
+  return () =>
+      i18next
+      .use(Backend)
+      .use(LanguageDetector)
+      .init({
+      resources,
+      supportedLngs: ['en', 'el'],
       fallbackLng: 'en',
       debug: true,
       returnEmptyString: false,
+      defaultNS: "translation",
       ns: [
-        'translation',
-        'validation',
-        'error',
+        'translation'
       ],
       interpolation: {
         format: I18NextModule.interpolationFormat(defaultInterpolationFormat)
       },
+      backend: {
+        loadPath: 'assets/locales/{{lng}}.{{ns}}.json',
+      },
+      // lang detection plugin options
+      detection: {
+        // order and from where user language should be detected
+        order: ['querystring', 'cookie'],
+        // keys or params to lookup language from
+        lookupCookie: 'lang',
+        lookupQuerystring: 'lng',
+        // cache user language on
+        caches: ['localStorage', 'cookie'],
+        // optional expire and domain for set cookie
+        cookieMinutes: 10080, // 7 days
+      }
     });
 }
 export function localeIdFactory(i18next: ITranslationService)  {
